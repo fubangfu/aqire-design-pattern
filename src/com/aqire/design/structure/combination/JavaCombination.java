a4aa9cb106f308e815cf1fa388b554b427472762
fubangfu <fubangfu@uservices.cn>
2020-06-09 17:48:52 +0800

    组合模式

---
+++ src/com/aqire/design/structure/combination/JavaCombination.java
@@ -0,0 +1,443 @@
+package com.aqire.design.structure.combination;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 组合模式
+ *
+ * @author fubangfu2015@163.com
+ * @date 2020/6/9 15:57
+ * --------------------------------------------
+ */
+
+// 组件接口会声明组合中简单和复杂对象的通用操作。
+interface Shape {
+    int getX();
+
+    int getY();
+
+    int getWidth();
+
+    int getHeight();
+
+    void move(int x, int y);
+
+    boolean isInsideBounds(int x, int y);
+
+    void select();
+
+    void unSelect();
+
+    boolean isSelected();
+
+    void paint(Graphics graphics);
+}
+
+abstract class BaseShape implements Shape {
+    public int x;
+    public int y;
+    public Color color;
+    private boolean selected = false;
+
+    BaseShape(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    @Override
+    public boolean isInsideBounds(int x, int y) {
+        return x > getX() && x < (getX() + getWidth()) &&
+                y > getY() && y < (getY() + getHeight());
+    }
+
+    @Override
+    public void select() {
+        selected = true;
+    }
+
+    @Override
+    public void unSelect() {
+        selected = false;
+    }
+
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    void enableSelectionStyle(Graphics graphics) {
+        graphics.setColor(Color.LIGHT_GRAY);
+
+        Graphics2D g2 = (Graphics2D) graphics;
+        float dash1[] = {2.0f};
+        g2.setStroke(new BasicStroke(1.0f,
+                BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER,
+                2.0f, dash1, 0.0f));
+    }
+
+    void disableSelectionStyle(Graphics graphics) {
+        graphics.setColor(color);
+        Graphics2D g2 = (Graphics2D) graphics;
+        g2.setStroke(new BasicStroke());
+    }
+
+
+    @Override
+    public void paint(Graphics graphics) {
+        if (isSelected()) {
+            enableSelectionStyle(graphics);
+        } else {
+            disableSelectionStyle(graphics);
+        }
+
+        // ...
+    }
+}
+
+// 叶节点类代表组合的终端对象。叶节点对象中不能包含任何子对象。叶节点对象
+// 通常会完成实际的工作，组合对象则仅会将工作委派给自己的子部件。
+class Dot extends BaseShape {
+    private final int DOT_SIZE = 3;
+
+    public Dot(int x, int y, Color color) {
+        super(x, y, color);
+    }
+
+    @Override
+    public int getWidth() {
+        return DOT_SIZE;
+    }
+
+    @Override
+    public int getHeight() {
+        return DOT_SIZE;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.fillRect(x - 1, y - 1, getWidth(), getHeight());
+    }
+}
+
+// 所有组件类都可以扩展其他组件。
+// 圆形
+class Circle extends BaseShape {
+    public int radius;
+
+    public Circle(int x, int y, int radius, Color color) {
+        super(x, y, color);
+        this.radius = radius;
+    }
+
+    @Override
+    public int getWidth() {
+        return radius * 2;
+    }
+
+    @Override
+    public int getHeight() {
+        return radius * 2;
+    }
+
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawOval(x, y, getWidth() - 1, getHeight() - 1);
+    }
+}
+
+// 三角形
+class Rectangle extends BaseShape {
+    public int width;
+    public int height;
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawRect(x, y, getWidth() - 1, getHeight() - 1);
+    }
+}
+
+// 组合类表示可能包含子项目的复杂组件。组合对象通常会将实际工作委派给子项
+// 目，然后“汇总”结果。
+class CompoundShape extends BaseShape {
+    protected List<Shape> children = new ArrayList<>();
+
+    public CompoundShape(Shape... components) {
+        super(0, 0, Color.BLACK);
+        add(components);
+    }
+
+    // 组合对象可在其项目列表中添加或移除其他组件（简单的或复杂的皆可）。
+    public void add(Shape component) {
+        children.add(component);
+    }
+
+    public void add(Shape... components) {
+        children.addAll(Arrays.asList(components));
+    }
+
+    public void remove(Shape child) {
+        children.remove(child);
+    }
+
+    public void remove(Shape... components) {
+        children.removeAll(Arrays.asList(components));
+    }
+
+    public void clear() {
+        children.clear();
+    }
+
+    @Override
+    public int getX() {
+        if (children.size() == 0) {
+            return 0;
+        }
+        int x = children.get(0).getX();
+        for (Shape child : children) {
+            if (child.getX() < x) {
+                x = child.getX();
+            }
+        }
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        if (children.size() == 0) {
+            return 0;
+        }
+        int y = children.get(0).getY();
+        for (Shape child : children) {
+            if (child.getY() < y) {
+                y = child.getY();
+            }
+        }
+        return y;
+    }
+
+    @Override
+    public int getWidth() {
+        int maxWidth = 0;
+        int x = getX();
+        for (Shape child : children) {
+            int childsRelativeX = child.getX() - x;
+            int childWidth = childsRelativeX + child.getWidth();
+            if (childWidth > maxWidth) {
+                maxWidth = childWidth;
+            }
+        }
+        return maxWidth;
+    }
+
+    @Override
+    public int getHeight() {
+        int maxHeight = 0;
+        int y = getY();
+        for (Shape child : children) {
+            int childsRelativeY = child.getY() - y;
+            int childHeight = childsRelativeY + child.getHeight();
+            if (childHeight > maxHeight) {
+                maxHeight = childHeight;
+            }
+        }
+        return maxHeight;
+    }
+
+    @Override
+    public void move(int x, int y) {
+        for (Shape child : children) {
+            child.move(x, y);
+        }
+    }
+
+    @Override
+    public boolean isInsideBounds(int x, int y) {
+        for (Shape child : children) {
+            if (child.isInsideBounds(x, y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void unSelect() {
+        super.unSelect();
+        for (Shape child : children) {
+            child.unSelect();
+        }
+    }
+
+    public boolean selectChildAt(int x, int y) {
+        for (Shape child : children) {
+            if (child.isInsideBounds(x, y)) {
+                child.select();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 组合会以特定的方式执行其主要逻辑。它会递归遍历所有子项目，并收集和
+    // 汇总其结果。由于组合的子项目也会将调用传递给自己的子项目，以此类推，
+    // 最后组合将会完成整个对象树的遍历工作。
+    @Override
+    public void paint(Graphics graphics) {
+        if (isSelected()) {
+            enableSelectionStyle(graphics);
+            graphics.drawRect(getX() - 1, getY() - 1, getWidth() + 1, getHeight() + 1);
+            disableSelectionStyle(graphics);
+        }
+
+        for (Shape child : children) {
+            child.paint(graphics);
+        }
+    }
+}
+
+// 客户端代码会通过基础接口与所有组件进行交互。这样一来，客户端代码便可同
+// 时支持简单叶节点组件和复杂组件。
+class ImageEditor {
+    private EditorCanvas canvas;
+    private CompoundShape allShapes = new CompoundShape();
+
+    public ImageEditor() {
+        canvas = new EditorCanvas();
+    }
+
+    public void loadShapes(Shape... shapes) {
+        allShapes.clear();
+        allShapes.add(shapes);
+        canvas.refresh();
+    }
+
+    private class EditorCanvas extends Canvas {
+        JFrame frame;
+
+        private static final int PADDING = 10;
+
+        EditorCanvas() {
+            createFrame();
+            refresh();
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    allShapes.unSelect();
+                    allShapes.selectChildAt(e.getX(), e.getY());
+                    e.getComponent().repaint();
+                }
+            });
+        }
+
+        void createFrame() {
+            frame = new JFrame();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+
+            JPanel contentPanel = new JPanel();
+            Border padding = BorderFactory.createEmptyBorder(PADDING, PADDING, PADDING, PADDING);
+            contentPanel.setBorder(padding);
+            frame.setContentPane(contentPanel);
+
+            frame.add(this);
+            frame.setVisible(true);
+            frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+        }
+
+        public int getWidth() {
+            return allShapes.getX() + allShapes.getWidth() + PADDING;
+        }
+
+        public int getHeight() {
+            return allShapes.getY() + allShapes.getHeight() + PADDING;
+        }
+
+        void refresh() {
+            this.setSize(getWidth(), getHeight());
+            frame.pack();
+        }
+
+        // 将所需组件组合为复杂的组合组件。
+        public void paint(Graphics graphics) {
+            // 所有组件都将被绘制。
+            allShapes.paint(graphics);
+        }
+    }
+}
+
+
+public class JavaCombination {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
---
+++ src/com/aqire/design/structure/decorate/JavaDecorate.java
@@ -0,0 +1,12 @@
+package com.aqire.design.structure.decorate;
+
+/**
+ * 装饰模式
+ *
+ * @author fubangfu2015@163.com
+ * @date 2020/6/9 17:18
+ * --------------------------------------------
+ */
+public class JavaDecorate {
+
+}
