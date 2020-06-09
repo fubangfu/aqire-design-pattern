3a09df36bd69cd14866a938b03ce8c93ae8ae31f
fubangfu <fubangfu@uservices.cn>
2020-06-09 14:39:13 +0800

    适配器模式

--- src/com/aqire/design/structure/adapter/JavaAdapter.java
+++ src/com/aqire/design/structure/adapter/JavaAdapter.java
@@ -8,6 +8,103 @@ package com.aqire.design.structure.adapter;
  * --------------------------------------------
  */
 
+// 假设你有两个接口相互兼容的类：圆孔（Round­Hole）和圆钉（Round­Peg）。
+class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    // 返回孔的半径。
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
+
+class RoundPeg {
+    private double radius;
+
+    public RoundPeg() {
+    }
+
+    public RoundPeg(double radius) {
+        this.radius = radius;
+    }
+
+    // 返回钉子的半径。
+    public double getRadius() {
+        return radius;
+    }
+}
+
+
+// 但还有一个不兼容的类：方钉（Square­Peg）。
+class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
+
+// 适配器类让你能够将方钉放入圆孔中。它会对 RoundPeg 类进行扩展，以接收适
+// 配器对象作为圆钉。
+class SquarePegAdapter extends RoundPeg {
+    // 在实际情况中，适配器中会包含一个 SquarePeg 类的实例。
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // 适配器会假扮为一个圆钉，
+        // 其半径刚好能与适配器实际封装的方钉搭配起来。
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
 
 public class JavaAdapter {
+    public static void main(String[] args) {
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
 }
